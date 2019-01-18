19f4fe19d8ea437e5f8224e4126963465ad09eb9
WingChhun <capt.wing.chhun@gmail.com>
2019-01-18 15:00:24 -0800

    install local storage package and load in store

--- src/redux/reducers/balance/balanceReducer.test.js
+++ src/redux/reducers/balance/balanceReducer.test.js
@@ -1,4 +1,5 @@
 import balanceReducer from "./balanceReducer";
+import balanceReducer2 from "./balanceReducer";
 import * as constants from "../../actions/balance/constants";
 import {withdrawBalance} from "../../actions/balance/balance";
 
@@ -13,6 +14,8 @@ const initialState = {
 
 describe("Reducer - Balance", () => {
 
+    const balance = 10;
+
     it("returns initialState correctly", () => {
 
         const action = {
@@ -23,21 +26,36 @@ describe("Reducer - Balance", () => {
 
     });
 
-    it("Sets a balance", () => {
+    describe(`When initializing`, () => {
 
-        const balance = 10;
-        const prevState = undefined;
-        const action = {
-            type: SET_BALANCE,
-            balance
-        };
-        const expected = {
-            ...initialState
-        };
+        it("Sets a balance", () => {
 
-        expected.balance = balance;
+            const prevState = undefined;
+            const action = {
+                type: SET_BALANCE,
+                balance
+            };
+            const expected = {
+                ...initialState
+            };
 
-        expect(balanceReducer(prevState, action)).toEqual(expected);
+            expected.balance = balance;
+
+            expect(balanceReducer(prevState, action)).toEqual(expected);
+        })
+
+        /*
+NOTE: This is the cookie balance Reducer
+*/
+
+        describe(`Re-initializing w/ cookies`, () => {
+
+            it(`Reads the balance from cookies`, () => {
+
+                expect(balanceReducer2(undefined, {})).toEqual(balance);
+            });
+        })
+
     })
 
     it(`deposits a balance`, () => {
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,9 +3,13 @@ import {composeWithDevTools} from 'redux-devtools-extension';
 import rootReducer from "./rootReducer";
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import {save, load} from 'redux-localstorage-simple';
 
+/*
+Note: save() middleware for localstorage: load initialState from lcoalSTorage if exists
+*/
 const middleware = [logger, thunk]
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)));
+const store = createStore(rootReducer, load(), composeWithDevTools(applyMiddleware(...middleware, save())));
 
 console.log("store", store.getState());
 export default store;
