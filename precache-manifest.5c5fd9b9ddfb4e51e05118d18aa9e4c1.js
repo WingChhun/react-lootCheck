13e1a2e6090e8daa4cc87dd6188eabb9436f463d
WingChhun <capt.wing.chhun@gmail.com>
2019-01-19 18:27:27 -0800

    Updates

---
+++ precache-manifest.5c5fd9b9ddfb4e51e05118d18aa9e4c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "9f4f7fa745136c77ea886c6070bb7485",
+    "url": "/static/media/background.9f4f7fa7.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9875755f48c960a61489",
+    "url": "/static/js/main.9875755f.chunk.js"
+  },
+  {
+    "revision": "1419d6286a8800cbb61b",
+    "url": "/static/js/1.1419d628.chunk.js"
+  },
+  {
+    "revision": "9875755f48c960a61489",
+    "url": "/static/css/main.0e73f41e.chunk.css"
+  },
+  {
+    "revision": "55ed7015ad48f1dd4c84309a4342c3bb",
+    "url": "/index.html"
+  }
+];
